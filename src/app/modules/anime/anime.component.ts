d084de23a7f3398377cfeb283a1b22c32752ce23
Ajay-Origin <87091017+Ajay-Origin@users.noreply.github.com>
2021-12-16 17:37:24 +0530

    Carousal changes

--- src/app/modules/anime/anime.component.ts
+++ src/app/modules/anime/anime.component.ts
@@ -13,15 +13,27 @@ export class AnimeComponent implements OnInit {
   public allTimeAiringanimeList: Anime[] = [];
   public numberOfSlides: number = 0;
   public animeItemsPerSlide: number = 6;
-  constructor(public animeApiService: AnimeService, router: Router) { }
+  public numbers: number[] = [];
+  constructor(public animeApiService: AnimeService, router: Router) {
+      this.numbers = Array(5).fill(0).map((x,i)=>i);
+   }
 
   ngOnInit(): void {
     this.loadAllTimeAiringAnime();
-    this.numberOfSlides = this.allTimeAiringanimeList.length % this.animeItemsPerSlide === 0 ? this.allTimeAiringanimeList.length / this.animeItemsPerSlide : (this.allTimeAiringanimeList.length / this.animeItemsPerSlide) + 1
+  }
+  ngDoCheck() {
+  this.numberOfSlides = this.allTimeAiringanimeList.length % this.animeItemsPerSlide === 0 ? this.allTimeAiringanimeList.length / this.animeItemsPerSlide : Math.floor(this.allTimeAiringanimeList.length / this.animeItemsPerSlide) + 1;
   }
   private loadAllTimeAiringAnime() {
     return this.animeApiService.getEmployees().subscribe((data: any) => {
       this.allTimeAiringanimeList = data.top as Anime[];
-    })
+    });
+  }
+  //function to return list of numbers from 0 to n-1
+  getNumberOfSlides(): Array<number> {
+    return Array(this.numberOfSlides);
+  }
+  getAnimeListForNthSlide(slideIndex: number) {
+    return this.allTimeAiringanimeList.slice(slideIndex * this.animeItemsPerSlide, (slideIndex * this.animeItemsPerSlide) + this.animeItemsPerSlide);
   }
 }
